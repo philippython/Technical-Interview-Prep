552d5b0030a379ee89c09fde13b0ede4dfe4c4d9
odulaja philip <odulajaphilip@gmail.com>
2022-08-07 04:55:22 +0100

    negative list slicing

--- sets.py
+++ sets.py
@@ -1,6 +1,10 @@
 new_set = set(("poland", "netherlands", "wroclaw", "warsaw", "monaco", "monaco"))
+new_set.add("luxoft")
+new_set.update(["Google", "Microsoft", "amazon"])
+print(new_set)
+new_set.remove("monaco")
 print(new_set)
 
-non_tuple = ("tuple")
-tuple = ("tuple",)
-print(type(non_tuple), type(tuple))
+non_tuple = set(("tuple", "amazon"))
+tuple = set(("tuple", "netflix"))
+print(f"{non_tuple  - tuple}")
--- tuples.py
+++ tuples.py
@@ -1,1 +1,15 @@
- 
+#create a tuple
+tuplex = ("w", "3", "r", "e", "s", "o", "u", "r", "c", "e") 
+print(tuplex.count("e"))
+#get item (4th element)of the tuple by index
+item = tuplex[3]
+print(item)
+
+#get item (4th element from last)by index negative
+item1 = tuplex[-4]
+print(item1)
+
+from copy import deepcopy
+
+deep_tuple = deepcopy(item)
+print("".join(tuplex))
