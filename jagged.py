f789b221ce8280c187c780e8c7d8aa330b7b4da4
odulaja philip <odulajaphilip@gmail.com>
2022-08-08 20:35:28 +0100

    linked list

---
+++ jagged.py
@@ -0,0 +1,9 @@
+counselors = []
+counselors = ['Adam','Bob','Carol','Denise','Eddie','Fiona','George']
+
+#create a list of Carol's activities 
+activities = []
+activities = ['tennis','dance','skating']
+
+counselors[2].append(activities)
+print(counselors)
