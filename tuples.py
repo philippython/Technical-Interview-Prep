669ba7fdcae777474a05dd1eff503a701b0dea5a
odulaja philip <odulajaphilip@gmail.com>
2022-08-26 16:00:58 +0100

    __init__

--- tuples.py
+++ tuples.py
@@ -13,3 +13,38 @@ from copy import deepcopy
 
 deep_tuple = deepcopy(item)
 print("".join(tuplex))
+
+# def is_valid_walk(walk):
+#     #determine if walk is valid
+#     walk_time = len(walk)
+#     routes = set(walk)
+#     print(routes)
+#     if walk_time <= 10:
+#         return True
+#     else:
+#         return False
+
+# def is_valid_walk(walk):
+#     #determine if walk is valid
+#     ns, ew = 0, 0
+#     if len(walk) == 10:
+#         for i in walk:
+#             if i == 'n': ns+=1
+#             if i == 's': ns-=1
+#             if i == 'w': ew+=1
+#             if i == 'e': ew-=1
+#     else:
+#         return False
+#     return ns == 0 and ew == 0
+
+from math import sqrt
+def find_next_square(sq):
+    # Return the next square if sq is a square, -1 otherwise
+    if isinstance(sq, int):
+        sqr = sqrt(sq)
+        next = sqr + 1
+        return  next ** next
+    else:
+        return -1
+
+print(find_next_square(1219))
