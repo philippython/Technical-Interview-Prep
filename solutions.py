7ac1abc34de608b64c3e249c707a4b5240b14dd1
odulaja philip <odulajaphilip@gmail.com>
2022-08-26 16:50:34 +0100

    cracked luxoft algorithm

--- solutions.py
+++ solutions.py
@@ -5,16 +5,19 @@
 #   if there are any (if there are not, an empty list shall be used as value for the specific keyword). 
 #  Anagrams already found for a specific word shall not be considered new keys in the dictionary.
 
-from tkinter import WORD
-
 
 string = "race bear thing care keen trap tweet earth knee heart neek tewt"
 
 def get_anagrams(string):
     words_list = string.split()
-    
+    output = {}
+    for word in words_list:
+        for figure in words_list:
+                if sorted(word) == sorted(figure) and len(word) == len(figure) and word != figure:
+                        output[word] = [figure]
+        if word not in output.keys():
+            output[word] = []
     print(output)
-    # print(final)
         
 
 get_anagrams(string)
